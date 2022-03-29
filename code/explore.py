e476b6c2c7c3c8fe52657bdfc7d6bacf61f0376f
BoundlessOne <spcwainiocyber@gmail.com>
2022-03-29 06:57:14 -0400

    Early morning revisions, scikitlearn histogram success, used plt.show
    new sampling method and bias examination to reduce error greatly
    
    next up is in depth data exploration

---
+++ code/explore.py
@@ -0,0 +1,9 @@
+# now we go a bit more in depth into the data set
+
+import numpy as np
+import pandas as pd
+import os
+from load import *
+
+data = load_housing_data()
+
--- code/splitdata.py
+++ code/splitdata.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from load import *
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import StratifiedShuffleSplit
 
 def split_train_test(data, test_ratio):
     shuffled_Indices = np.random.permutation(len(data))
@@ -22,10 +22,33 @@ train_set, test_set = train_test_split(data, test_size=0.2, random_state=42)
 #median income is one such variable. Lets cut it into representative categories
 
 #brief chek
-print(len(train_set))
-print(len(test_set))
-print(len(data))
+#print(len(train_set))
+#print(len(test_set))
+#print(len(data))
 
-data["income_cat"] = pd.cut(data["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1,2,3,4,5])
+data["income_cat"] = pd.cut(data["median_income"], bins=[0., 1.5, 3.0, 4.5, 6., np.inf], labels=[1, 2, 3, 4, 5])
+#print(type(data["income_cat"]))
+#print(data["income_cat"], data["median_income"])
+#data["income_cat"].value_counts()
+#data["income_cat"].hist()
+#plt.show()
 
-data.hist(column='income_cat')
+# stratified sampling based on category, such that no one category is over represented and our model does not overtrain
+# certain categories
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+#print(split)
+for train_index, test_index in split.split(data, data["income_cat"]):
+    strat_train_set = data.loc[train_index]
+    strat_test_set = data.loc[test_index]
+
+#print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+
+#the sampling bias comparison of overall, stratified, and random sampling reveals that the stratified sampling meth0d
+# yields an order of magnitude less sampling error.
+
+#remove the income_cat to return data to original state
+
+#for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
